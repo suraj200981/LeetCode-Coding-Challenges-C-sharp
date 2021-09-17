dbf28e975b0ace64d3e02c1941491dc6ae9fe5b7
ss2261 <ss2261@kent.ac.uk>
2021-09-17 12:43:02 +0100

    Add project files.

---
+++ LeetCode Coding Challenges/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace LeetCode_Coding_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = new int[] { 3, 2, 4 };
+
+            Console.WriteLine("Hello World!");
+
+            var TwoSumInstance = new TwoSum();
+            //converting int array to string, can remove from leetcode after
+            string result = string.Join(",", TwoSumInstance.TwoSumFunc(nums, 6));
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LeetCode Coding Challenges/TwoSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Coding_Challenges
+{
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+   You may assume that each input would have exactly one solution, and you may not use the same element twice.
+   You can return the answer in any order.*/
+
+   public class TwoSum
+    {
+
+        public int[] TwoSumFunc(int[] nums, int target)
+        {
+            int[] inputArr = nums;
+            int inputTargetNum = target;
+
+            int[] resultArr = new int[2];
+
+            for (int x = 0; x < inputArr.Length; x++) {
+                for (int i = x+1; i < inputArr.Length; i++)
+                {
+                    if (inputArr[x] + inputArr[i]==inputTargetNum) {
+
+                        resultArr[0] = x;
+                        resultArr[1] = i;
+
+                       return resultArr;
+                    }
+                }
+            }
+            return resultArr;
+        }
+    }
+}
