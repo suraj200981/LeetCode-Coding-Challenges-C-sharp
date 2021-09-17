8b27427b61d24bc3b1cf54c69adcb5cefb2940d2
ss2261 <ss2261@kent.ac.uk>
2021-09-17 14:52:26 +0100

    reverse any 32 bit integer - nearly completed

--- LeetCode Coding Challenges/Program.cs
+++ LeetCode Coding Challenges/Program.cs
@@ -8,15 +8,9 @@ namespace LeetCode_Coding_Challenges
         static void Main(string[] args)
         {
 
-            int[] nums = new int[] { 3, 2, 4 };
-
-            Console.WriteLine("Hello World!");
-
-            var TwoSumInstance = new TwoSum();
-            //converting int array to string, can remove from leetcode after
-            string result = string.Join(",", TwoSumInstance.TwoSumFunc(nums, 6));
-            Console.WriteLine(result);
-            Console.ReadLine();
+            var testInstance = new ReverseInteger();
+            testInstance.Reverse(-123);
+            
 
         }
     }
---
+++ LeetCode Coding Challenges/ReverseInteger.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Coding_Challenges
+{
+ /* Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+    Assume the environment does not allow you to store 64-bit integers(signed or unsigned).*/
+
+/*    Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+Example 4:
+
+Input: x = 0
+Output: 0*/
+
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            StringBuilder sb = new StringBuilder();
+            string str = x.ToString();
+
+            bool negativeNumber;
+            if (str[0] == '-')
+            {
+                negativeNumber = true;
+                str = str.Replace("-", "");
+
+            }
+            else {
+                negativeNumber = false;
+                str.Replace("-", "");
+            }
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+
+            for (int i = 0; i < charArray.Length; i++) {
+
+                sb.Append(charArray[i]);
+            }
+
+
+
+
+            if (negativeNumber)
+            {
+                Console.WriteLine(-Int32.Parse(sb.ToString()));
+
+            }
+            else {
+                Console.WriteLine(Int32.Parse(sb.ToString()));
+
+            }
+
+           
+            Console.ReadLine();
+
+           
+            return 0;
+        }
+
+    }
+}
