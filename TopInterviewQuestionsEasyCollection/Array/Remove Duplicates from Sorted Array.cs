9687f7e42897a252f5ac57ef7723ebf392fd88a1
suraj <suraj200981@yahoo.com>
2021-09-19 10:18:14 +0100

    remove duplicates challenge started

---
+++ TopInterviewQuestionsEasyCollection/Array/Remove Duplicates from Sorted Array.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TopInterviewQuestionsEasyCollection;
+namespace TopInterviewQuestionsEasyCollection.Array
+{
+    public class Remove_Duplicates_from_Sorted_Array
+    { 
+        public int RemoveDuplicates(int[] nums)
+        {
+            Console.WriteLine("hello world test");
+            return 0;
+        }
+
+
+    }
+}   
