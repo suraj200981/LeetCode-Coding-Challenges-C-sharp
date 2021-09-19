b6040105aeaf507a5735abda05ce9b35a2210f65
suraj <suraj200981@yahoo.com>
2021-09-19 11:32:34 +0100

    function completed, now need to test on LeetCode

--- TopInterviewQuestionsEasyCollection/Program.cs
+++ TopInterviewQuestionsEasyCollection/Program.cs
@@ -8,7 +8,7 @@ namespace TopInterviewQuestionsEasyCollection
         static void Main(string[] args)
         {
 
-            int[] nums = new int[] { 1, 2, 3, 4, 5 };
+            int[] nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3,3,3, 3, 4 };
             var testInstance = new Array.Remove_Duplicates_from_Sorted_Array();
 
 
