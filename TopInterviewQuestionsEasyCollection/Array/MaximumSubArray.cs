71932b332fa7c96b0df1e78c7a241b28c967d721
suraj <suraj200981@yahoo.co.uk>
2021-09-21 18:10:38 +0100

    commit

---
+++ TopInterviewQuestionsEasyCollection/Array/MaximumSubArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TopInterviewQuestionsEasyCollection.Array
+{
+    public class MaximumSubArray
+    {
+        public MaximumSubArray()
+        {
+        }
+
+        public int MaxSubArray(int[] nums)
+        {
+            return 5;
+        }
+    }
+}
