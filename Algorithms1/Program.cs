ac2f673df29720be4ab6013ab86734bd2e4f4a2d
suraj200981 <suraj200981@yahoo.co.uk>
2021-12-21 19:36:32 +0000

    Runtime: 156 ms, faster than 66.77% of C# online submissions for Squares of a Sorted Array.

---
+++ Algorithms1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Algorithms1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testInstance = new SquaresofaSortedArray();
+
+            int[] nums = new int[] { -4, -1, 0, 3, 10 };
+            testInstance.SortedSquares(nums);
+
+        }
+    }
+}
---
+++ Algorithms1/SquaresofaSortedArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Algorithms1
+{
+
+    //this challenge can be found @ https://leetcode.com/problems/squares-of-a-sorted-array/submissions/
+    public class SquaresofaSortedArray
+    {
+
+        public int[] SortedSquares(int[] nums)
+        {
+            int[] newArr = new int[nums.Length];
+            //first we find the squares of each element in the array
+            for (int x=0; x<nums.Length; x++) {
+                //check if number is negative
+                    newArr[x] = (int)Math.Abs(Math.Pow(nums[x], 2));
+            }
+            Array.Sort(newArr);
+
+            for (int x = 0; x < newArr.Length; x++)
+            {
+                //check if number is negative
+                return newArr;
+            }
+
+            Console.Read();
+            return null;
+        }
+
+    }
+}
