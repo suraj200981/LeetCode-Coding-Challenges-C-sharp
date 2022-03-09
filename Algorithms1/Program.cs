6ff25e5b3abae556578d0ded5f2352b122c659f9
suraj200981 <suraj200981@yahoo.co.uk>
2022-03-09 14:51:04 +0000

    zigzag conversion - made progress on example 1 with number of rows as 3, need to fix for N num of rows

--- Algorithms1/Program.cs
+++ Algorithms1/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 
+
 namespace Algorithms1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var testInstance = new MoveZero();
 
-            int[] nums = new int[] { 0, 1, 0, 3, 12 };
-            testInstance.MoveZeroes(nums);
+
+class Program
+    {
+        public class ZigzagConversion {
+
+            public string Convert(string s, int numRows)
+            {
+                int count = 0;
+                
+                    for (int x = 0; x < s.Length; x++) {
+
+                        if (count == numRows)
+                        {
+                            break;
+                        }
+                        if (x %2==0) {
+                            for (int k=x; k<s.Length; k+=numRows+1) {
+                                Console.Write(s[k] + " ");
+
+                            }
+                            count++;
+                            
+                        }
+                        if (x %2!=0)
+                        {
+                            for (int k = x; k < s.Length; k += numRows - 1)
+                            {
+                                Console.Write(s[k] + " ");
+
+                            }
+                            count++;
+                        }
+
+                        Console.WriteLine();
+
+
+                    }
+
+                return null;
+            }
+        }
+   public static void Main(string[] args)
+        {
+            var testInstance = new ZigzagConversion();
+
+            string str = "PAYPALISHIRING";
+
+
+            Console.WriteLine(testInstance.Convert(str, 4));
+            Console.ReadLine();
 
         }
     }
---
+++ Algorithms1/ValidParentheses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms1
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            for (int x = 0; x < s.Length; x++) {
+
+                if (s.Length ==2 && (s[x] == '(' && s[x + 1] != ')') || (s[x] == '{' && s[x + 1] != '}') || (s[x] == '[' && s[x + 1] != ']')) {
+
+                    return false;
+                }
+                else if (x != s.Length - 1 && (s[x] == '(' && s[x + 1] == ')') || (s[x] == '{' && s[x + 1] == '}') || (s[x] == '[' && s[x + 1] == ']'))
+                {
+                    return true;
+                }
+                else if (x != s.Length - 1 && (s[x + 1] == '(' && s[x + 2] == ')') || (s[x + 1] == '{' && s[x + 2] == '}') || (s[x + 1] == '[' && s[x + 2] == ']')) {
+                    return true;
+
+                }
+                /*         else if (s[x] == ')')
+                         {
+                             return false;
+
+                         }*/
+                else
+                {
+                    return false;
+                }
+
+            }
+            return false;
+        }
+    }
+}
